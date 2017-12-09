531565770961c29187809eea194e4a6cba3a631d
reply-1988 <reply-1988@users.noreply.github.com>
2017-12-09 15:04:49 +0800

    数据库迁移文件

--- biaodan.py
+++ biaodan.py
@@ -1,23 +1,41 @@
 from datetime import datetime
 
 import os
-from flask import Flask, render_template, session, redirect, url_for, flash
+from flask import Flask, render_template, session, redirect, url_for, flash, jsonify
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
+from flask_script import Shell, Manager
+from flask_migrate import Migrate, MigrateCommand
+from flask_mail import Mail
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+# app的相关配置
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to string'
 app.config['SQLALCHEMY_DATABASE_URI'] =\
     'sqlite:///' + os.path.join(basedir, 'date.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['MAIL_SERVER'] = 'smtp.qq.com'
+app.config['MAIL_PORT'] = 587
+# app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 1271997525
+app.config['MAIL_PASSWORD'] = 'sodvdrykulkeiggg'
+
+# 数据库处理
 db = SQLAlchemy(app)
+# 模板处理
 bootstrap = Bootstrap(app)
+# 时间处理
 moment = Moment(app)
+# shell工具处理
+manager = Manager(app)
+# 邮箱处理
+mail = Mail(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -38,7 +56,8 @@ def index():
         session['name'] = form.name.data
         form.name.data = ''
         return redirect(url_for('index'))
-    return render_template('index.html', form = form, name = session.get('name'), current_time = datetime.utcnow())
+    return render_template('index.html', form = form, name = session.get('name'), known = session.get('known', False),
+                           current_time = datetime.utcnow())
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
@@ -62,5 +81,13 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.username
 
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role)
+manager.add_command("shell", Shell(make_context=make_shell_context))
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
+
+
 if __name__ == '__main__':
-    app.run()
+    manager.run()
+
